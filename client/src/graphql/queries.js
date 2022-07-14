f2f02fd6148a24f124de8dbb32224697394775cb
Brainybrian316 <brainybrian316@gmail.com>
2022-07-14 14:49:24 -0400

    graphql request client

---
+++ client/src/graphql/queries.js
@@ -0,0 +1,21 @@
+import { request, gql } from 'graphql-request';
+
+const GRAPH_URL = 'http://localhost:9000/graphql';
+
+export async function getJobs() {
+  const query = gql`
+    query {
+  jobs {
+    id
+    title
+    company {
+      name
+    }
+  }
+}
+  `;
+  const data = await request(GRAPH_URL, query);
+  console.log('data', data);
+
+
+}
