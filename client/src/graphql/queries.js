f948e50a9ae7049929dae44e60cc217b7db9e12c
Brainybrian316 <brainybrian316@gmail.com>
2022-07-14 15:41:54 -0400

    query variables

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -5,17 +5,34 @@ const GRAPH_URL = 'http://localhost:9000/graphql';
 export async function getJobs() {
   const query = gql`
     query {
-  jobs {
-    id
-    title
-    company {
-      name
+      jobs {
+        id
+        title
+        company {
+          name
+        }
+      }
     }
-  }
-}
   `;
   const { jobs } = await request(GRAPH_URL, query);
-  return jobs
+  return jobs;
+}
 
-
+export async function getJob(id) {
+  const query = gql`
+    query JobQuery($id: ID!) {
+      job(id: $id) {
+        id
+        title
+        company {
+          id
+          name
+        }
+        description
+      }
+    }
+  `;
+  const variables = { id };
+  const { job } = await request(GRAPH_URL, query, variables);
+  return job;
 }
