e03ade8ba55879a0ee542d76cb2da3980be6b9e3
Brainybrian316 <brainybrian316@gmail.com>
2022-07-14 14:27:24 -0400

    job company associations

--- server/resolvers.js
+++ server/resolvers.js
@@ -5,7 +5,10 @@ export const resolvers = {
     jobs: () => Job.findAll(),
   },
 
+  // looks in Job type query for the company field
   Job: {
+    // using company db we use job argument to get information from job db 
     company: (job) => Company.findById(job.companyId)
+    // find a company id using the company db to attach to job query
   },
 }
