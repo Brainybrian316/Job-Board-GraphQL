382e4a2cdad6a5c3e2bb31a24c66d8c8c1de3ee7
Brainybrian316 <brainybrian316@gmail.com>
2022-07-14 15:28:12 -0400

    query arguments

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,16 +1,18 @@
 import { Company, Job } from './db.js';
 
-
 export const resolvers = {
-  // we say jobs because we are querying the jobs table in schema.graphql
   Query: {
+    /* we only want one job from the job schema in graphql. we pass 'id' as an args instead of args since it contains the id and we only want the id */
+    job: (_root, { id }) => Job.findById(id),
+
+    // we say jobs because we are querying the jobs table in schema.graphql
     jobs: () => Job.findAll(), // db from jobs.json
   },
 
   // we search schema.graphql Job type for the fields we want to add
   Job: {
     // we want to add a company field to the Job type so we pass job as the first argument to the function
-    company: (job) => Company.findById(job.companyId)
-    // in the companies.json we find the company with the id of the job 
+    company: (job) => Company.findById(job.companyId),
+    // in the companies.json we find the company with the id of the job
   },
-}
+};
