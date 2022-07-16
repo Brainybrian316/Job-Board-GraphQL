b96505243dcc53a6fd2c3c7f3f7814b2a101ddc3
Brainybrian316 <brainybrian316@gmail.com>
2022-07-15 21:23:35 -0400

    cache manipulation

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -7,20 +7,50 @@ const GRAPH_URL = 'http://localhost:9000/graphql';
 const client = new ApolloClient({
   uri: GRAPH_URL,
   cache: new InMemoryCache(),
+
 })
+
+const JOB_QUERY = gql`
+query JobQuery($id: ID!) {
+  job(id: $id) {
+    id
+    title
+    company {
+      id
+      name
+    }
+    description
+  }
+}
+`;
 
 export async function createJob(input) {
   const mutation = gql`
   mutation CreateJobMutation($input: CreateJobInput!) {
   job: createJob(input: $input) {
     id
+    title 
+    company {
+      id 
+      name 
+    }
+    description
   }
 }
   `;
   const variables = { input };
   const clientContext = {
     headers: { Authorization: `Bearer ${getAccessToken() }` }, };
-    const { data: { job } } = await client.mutate({ mutation, variables, clientContext });
+    const { data: { job } } = await client.mutate({ 
+      mutation, variables, clientContext,
+      update: (cache, { data: { job } }) => {
+      cache.writeQuery({
+        query: JOB_QUERY,
+        variables: { id: job.id },
+        data: { job },
+      });
+     },
+    });
     return job;
   };
 
@@ -50,31 +80,20 @@ export async function getJobs() {
         id
         title
         company {
+          id
           name
         }
       }
     }
   `;
-  const { data: { jobs }} = await client.query({ query });
+  const { data: { jobs }} = await client.query({ query, fetchPolicy: 'network-only' });
   return jobs;
 }
 
 export async function getJob(id) {
-  const query = gql`
-    query JobQuery($id: ID!) {
-      job(id: $id) {
-        id
-        title
-        company {
-          id
-          name
-        }
-        description
-      }
-    }
-  `;
+  
   const variables = { id };
-  const { data: { job } } = await client.query({ query, variables });
+  const { data: { job } } = await client.query({ query: JOB_QUERY, variables });
   return job;
 }
 
